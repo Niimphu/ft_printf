c8109ee980a8fd7c7afcaf2adb153b14fde7f477
yiwong <yiwong@student.42wolfsburg.de>
2023-01-28 16:45:04 +0100

    nullpointerfix

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 19:30:01 by yiwong            #+#    #+#             */
-/*   Updated: 2023/01/28 01:30:30 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/01/28 16:42:25 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,33 +23,57 @@ int	ft_printf_s(char *s)
 	return (r);
 }
 
-int	ft_printf_base(unsigned int long long u, int len, char *set, int base)
+int	ft_printf_int(int n, int len)
 {
-	if (u == 0)
+	char	c;
+
+	if (n == -2147483648)
+		return (ft_printf_s("-2147483648"));
+	if (n < 0)
+	{
+		n *= -1;
+		len += write(1, "-", 1);
+	}
+	if (n == 0)
 		return (len);
 	else
-		len = ft_printf_base(u / base, len, set, base);
-	write(1, &set[(u % base)], 1);
-	len++;
+		len = ft_printf_int(n / 10, len);
+	c = n % 10 + '0';
+	len += write(1, &c, 1);
+	return (len);
+}
+
+int	ft_printf_base(unsigned long long u, int len, char *set, int base)
+{
+	if (u)
+		len = ft_printf_base(u / base, ++len, set, base);
+	else
+	{
+		if (len == 0)
+		{
+			ft_putchar_fd('0', 1);
+			return (1);
+		}
+		return (len);
+	}
+	ft_putchar_fd(set[(u % base)], 1);
 	return (len);
 }
 
 int	ft_printf_fmt(const char fmt, va_list *args)
 {
-	int	c;
-
 	if (fmt == 'c')
 	{
-		c = va_arg(*args, int);
-		return (write(1, &c, 1));
+		ft_putchar_fd(va_arg(*args, int), 1);
+		return (1);
 	}
 	if (fmt == 's')
 		return (ft_printf_s(va_arg(*args, char *)));
 	if (fmt == 'p')
 		return (ft_printf_s("0x") + ft_printf_base(va_arg(*args, \
-			unsigned int long long), 0, "0123456789abcdef", 16));
+			unsigned long long), 0, "0123456789abcdef", 16));
 	if (fmt == 'd' || fmt == 'i')
-		return (ft_printf_s(ft_itoa(va_arg(*args, int))));
+		return (ft_printf_int(va_arg(*args, int), 0));
 	if (fmt == 'u')
 		return (ft_printf_base(va_arg(*args, unsigned int), \
 			0, "0123456789", 10));
@@ -57,7 +81,7 @@ int	ft_printf_fmt(const char fmt, va_list *args)
 		return (ft_printf_base(va_arg(*args, unsigned int), \
 			0, "0123456789abcdef", 16));
 	if (fmt == 'X')
-		return (ft_printf_base(va_arg(*args, unsigned int long long), \
+		return (ft_printf_base(va_arg(*args, unsigned long long), \
 			0, "0123456789ABCDEF", 16));
 	ft_putchar_fd('%', 1);
 	return (1);
@@ -97,10 +121,10 @@ int	ft_printf(const char *str, ...)
 // 	s = "Hello World. )OIWEFRUWISUHRGKLBSKhgq3p894tgjrpur3pw84ugf. Yes.";
 // 	u = 4294967295;
 // 	x = 4294967295;
-// r1 = ft_printf("My print:\nc:%c\ns:%s\np:%p\nu:%u\n", c, s, s, u);
-// r1 += ft_printf("i:%i\nd:%d\nx:%x\nX:%X\n%%:%%\n\n", n, n, x, x);
-// r2 = printf("Standard:\nc:%c\ns:%s\np:%p\nu:%u\n", c, s, s, u);
-// r2 += printf("i:%i\nd:%d\nx:%x\nX:%X\n%%:%%\n\n", n, n, x, x);
+// 	r1 = ft_printf("My print:\nc:%c\ns:%s\np:%p\nu:%u\n", c, s, NULL, u);
+// 	r1 += ft_printf("i:%i\nd:%d\nx:%x\nX:%X\n%%:%%\n\n", n, n, x, x);
+// 	r2 = printf("Standard:\nc:%c\ns:%s\np:%p\nu:%u\n", c, s, NULL, u);
+// 	r2 += printf("i:%i\nd:%d\nx:%x\nX:%X\n%%:%%\n\n", n, n, x, x);
 // 	printf("ft_printf returns: %i\nprintf returns: %i\n", r1, r2);
 // 	return (0);
 // }
